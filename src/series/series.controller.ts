09785b346c0de0e1f23bfe0ee340ce8bb82f11be
nicole branco <hyaluna@gmail.com>
2022-05-25 20:26:31 -0300

    fix series controller

--- src/series/series.controller.ts
+++ src/series/series.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SeriesService } from './series.service';
 import { CreateSeriesDto } from './dto/create-series.dto';
 import { UpdateSeriesDto } from './dto/update-series.dto';
@@ -19,16 +27,16 @@ export class SeriesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.seriesService.findOne(+id);
+    return this.seriesService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateSeriesDto: UpdateSeriesDto) {
-    return this.seriesService.update(+id, updateSeriesDto);
+    return this.seriesService.update(id, updateSeriesDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.seriesService.remove(+id);
+    return this.seriesService.remove(id);
   }
 }
