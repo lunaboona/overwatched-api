ec1e1df6c8cce48daeda8fd1ed7c545750538601
nicole branco <hyaluna@gmail.com>
2022-06-01 20:05:16 -0300

    add db name

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    MongooseModule.forRoot('mongodb://localhost:27017/overwatched'),
     SeriesModule,
     SeasonsModule,
     EpisodesModule,
